d2599c9ab420a36154bac914a568029d761d8ae2
YunShin <2younsin@gmail.com>
2023-02-14 16:40:46 +0900

    initial commit

---
+++ src/main/java/shuttlebus/shuttlescore_v2_backend/ShuttlescoreV2BackendApplication.java
@@ -0,0 +1,13 @@
+package shuttlebus.shuttlescore_v2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuttlescoreV2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShuttlescoreV2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shuttlebus/shuttlescore_v2_backend/ShuttlescoreV2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package shuttlebus.shuttlescore_v2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuttlescoreV2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
